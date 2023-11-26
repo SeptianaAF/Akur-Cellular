c3058122694e667c3d26acebb818f905e852af63
SeptianaAF <148734283+SeptianaAF@users.noreply.github.com>
2023-11-26 12:29:45 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,2 @@
-$(document).ready(function () {
-    var darkModeEnabled = localStorage.getItem("darkModeEnabled");
-    if (darkModeEnabled === "true") {
-        $('body').addClass('dark-mode');
-        $('#septiana-theme-toggle').prop('checked', true);
-    }
-
-    $('#septiana-theme-toggle').change(function () {
-        var isChecked = this.checked;
-        $('body').toggleClass('dark-mode', isChecked);
-        localStorage.setItem("darkModeEnabled", isChecked);
-    });
-});
+alert("Disarankan menggunanakan mode desktop dan rasio landscape");
+document.getElementById("demo").innerHTML = "My First JavaScript";
